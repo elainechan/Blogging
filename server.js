65faf067cca6c12d24e08c48ef27f17649e9924c
elainechan <elaine.chan@outlook.com>
2018-02-25 03:14:27 -0500

    Adds `runServer()` and `closeServer()`.

--- server.js
+++ server.js
@@ -14,3 +14,32 @@ app.post('/blog-posts');
 app.put('/blog-posts/:id');
 app.delete('/blog-posts/:id');
 
+let server;
+function runServer() {
+	const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject) => {
+		server = app.listen(port, () => {
+			console.log(`Your app is listening at port ${port}...`);
+			resolve(server);
+		}).on('error', err => {
+			reject(err);
+		});
+	});
+}
+function closeServer() {
+	return new Promise((resolve, reject) => {
+		console.log(`Closing server...`);
+		server.close(err => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
